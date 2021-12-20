8d83f4e13da0ea76bfa28af4fb6a34d189b9b005
Wiz1991 <wizoftime92@gmail.com>
2021-12-20 15:51:32 +0100

    fix names

--- scripts/1_build_optimize_all.sh
+++ scripts/1_build_optimize_all.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -o errexit -o nounset -o pipefail
 command -v shellcheck > /dev/null && shellcheck "$0"
 
@@ -12,6 +12,8 @@ for DIR in contracts packages; do
         if [ -d "$D" ]; then
             (
                 cd "$D"
+                BASENAME="$(basename "$D")"
+                echo $BASENAME
                 echo "Building $D"
                 cargo build --release --target wasm32-unknown-unknown
                 echo "Building complete"
@@ -23,9 +25,10 @@ for DIR in contracts packages; do
                      enigmampc/secret-contract-optimizer:1.0.5
 
                 echo "Optimizing complete"    
+
                 echo "Copying artifacts"
-                name = $($D | tr "/")
-                cp *.wasm.gz artifacts/"$name".wasm.gz
+                # copy artifacts to artifacts dir with subdir as name
+                cp *.wasm.gz ../../artifacts/"$BASENAME"
                 echo "Copying complete"
             )
         fi
