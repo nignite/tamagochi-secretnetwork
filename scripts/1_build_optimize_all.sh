bfd9124a6c28c7b2cf7ae9adcfb06516fdf78824
Wiz1991 <wizoftime92@gmail.com>
2021-12-20 15:22:20 +0100

    change optimizer version

--- scripts/1_build_optimize_all.sh
+++ scripts/1_build_optimize_all.sh
@@ -21,7 +21,7 @@ for DIR in contracts packages; do
                 docker run --rm -v "$(pwd)":/contract \
                     --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
                     --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-                     enigmampc/secret-contract-optimizer  
+                     enigmampc/secret-contract-optimizer:1.0.5
 
                 echo "Optimizing complete"    
             )
